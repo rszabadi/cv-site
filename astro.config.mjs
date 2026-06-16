aa402f3d5753482fe5b1e01f4303e76248d3f0db
John Doe <johndoe@example.com>
2026-06-16 17:52:37 +0200

    fix netlify base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://rszabadi.github.io/cv-site',
-  base: '/cv-site/',
+  site: 'https://rmihai.netlify.app',
+  base: '/',
 });
